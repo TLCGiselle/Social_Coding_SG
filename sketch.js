5c7f5bc3e17fc19a76a0b86e0d958b0d600c803e
Giselle Morales <143736898+TLCGiselle@users.noreply.github.com>
2024-06-04 11:33:00 -0700

    hi hi

--- sketch.js
+++ sketch.js
@@ -1,7 +1,30 @@
+let aurora;
+let eva;
+let habanero;
+let hr;
+let patchwork;
+let pochi;
+let seadog;
+
+let squishyfont, handfont;
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(250);
+}
+
+function preload(){
+aurora= loadImage('Squishy Grip Asset\Aurora2 (1).png')
+eva = loadImage("Squishy Grip Asset/Eva2 (1).png")
+habanero = loadImage("Squishy Grip Asset/Habanero2 (1).png")
+hr= loadImage('Squishy Grip Asset\Aurora2 (1).png')
+patchwork = loadImage("Squishy Grip Asset/patch work.png")
+pochi= loadImage("Squishy Grip Asset\Aurora2 (1).png")
+seadog= loadImage('Squishy Grip Asset\Aurora2 (1).png')
+
+squishyfont = loadFont("Squishy Grip Asset/SquishyGrip-Regular (2).ttf")
+//handfont = 
 }
