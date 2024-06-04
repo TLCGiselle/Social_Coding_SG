6aa776d19a0a20172b1f4ca9d54f9ddb49608529
Giselle Morales <143736898+TLCGiselle@users.noreply.github.com>
2024-06-04 11:44:20 -0700

    new
    :D

--- sketch.js
+++ sketch.js
@@ -10,21 +10,21 @@ let squishyfont, handfont;
 
 function setup() {
   createCanvas(400, 400);
+  background(255);
 }
 
 function draw() {
-  background(250);
+  background(220);
 }
 
 function preload(){
-aurora= loadImage('Squishy Grip Asset\Aurora2 (1).png')
-eva = loadImage("Squishy Grip Asset/Eva2 (1).png")
-habanero = loadImage("Squishy Grip Asset/Habanero2 (1).png")
-hr= loadImage('Squishy Grip Asset\Aurora2 (1).png')
-patchwork = loadImage("Squishy Grip Asset/patch work.png")
-pochi= loadImage("Squishy Grip Asset\Aurora2 (1).png")
-seadog= loadImage('Squishy Grip Asset\Aurora2 (1).png')
-
-squishyfont = loadFont("Squishy Grip Asset/SquishyGrip-Regular (2).ttf")
-//handfont = 
+aurora= loadImage('Aurora2 (1).png')
+eva = loadImage("Eva2 (1).png")
+habanero = loadImage("Habanero2 (1).png")
+hr= loadImage("HR2 (1).png")
+patchwork = loadImage("patch work.png")
+pochi= loadImage("Pochi2 (1).png")
+seadog= loadImage("Seadog2 (1).png")
+squishyfont = loadFont("SquishyGrip-Regular (2).ttf")
+handfont= loadFont('KatHandwritten-Regular (1).ttf')
 }
