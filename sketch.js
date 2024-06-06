6a4e1949bbf26a62d6b9f7dfd5f8245cb7bb2588
Kat <kwright17@go.pasadena.edu>
2024-06-06 09:58:28 -0700

    organizing 👍

--- sketch.js
+++ sketch.js
@@ -1,3 +1,4 @@
+//Images
 let aurora;
 let eva;
 let habanero;
@@ -5,9 +6,24 @@ let hr;
 let patchwork;
 let pochi;
 let seadog;
+let logo;
 
+//Fonts
 let squishyfont, handfont;
 
+function preload(){
+  aurora= loadImage('Aurora2 (1).png')
+  eva = loadImage("Eva2 (1).png")
+  habanero = loadImage("Habanero2 (1).png")
+  hr= loadImage("HR2 (1).png")
+  patchwork = loadImage("patch work.png")
+  pochi= loadImage("Pochi2 (1).png")
+  seadog= loadImage("Seadog2 (1).png")
+  logo = loadImage("DMA98_FinalfinalSquishyGrip.png")
+  squishyfont = loadFont("SquishyGrip-Regular (2).ttf")
+  handfont= loadFont('KatHandwritten-Regular (1).ttf')
+  }
+  
 function setup() {
   createCanvas(400, 400);
   background(255);
@@ -15,16 +31,8 @@ function setup() {
 
 function draw() {
   background(220,0,0);
+
 }
 
-function preload(){
-aurora= loadImage('Aurora2 (1).png')
-eva = loadImage("Eva2 (1).png")
-habanero = loadImage("Habanero2 (1).png")
-hr= loadImage("HR2 (1).png")
-patchwork = loadImage("patch work.png")
-pochi= loadImage("Pochi2 (1).png")
-seadog= loadImage("Seadog2 (1).png")
-squishyfont = loadFont("SquishyGrip-Regular (2).ttf")
-handfont= loadFont('KatHandwritten-Regular (1).ttf')
-}
+
+
